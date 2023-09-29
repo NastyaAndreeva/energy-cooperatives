3299a1497a2606b0449fc76fcf12827834f6750b
NastyaAndreeva <andreeva.anastasiya.v@gmail.com>
2023-09-29 22:51:38 +0300

    updated settings

--- energy_cooperatives/mysite/views.py
+++ energy_cooperatives/mysite/views.py
@@ -1,3 +1,6 @@
+# views.py
 from django.shortcuts import render
-
-# Create your views here.
+# example
+def fruit_list(request):
+    fruits = ['Apple', 'Banana', 'Cherry', 'Date']
+    return render(request, 'index.html', {'fruits': fruits})
